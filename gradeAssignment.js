320b2641de47a2978428589a2cb6d3c1b80eb081
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 19:16:00 +0800

    Create gradeAssignment.js

---
+++ gradeAssignment.js
@@ -0,0 +1,18 @@
+// IF-ELSE-IF ACTIVITY
+
+// Create a program that prompts the user to input their score
+
+let score = prompt("Enter your score:");
+
+// Use else if statements to determine the user input, then log the corresponding grade to the console
+
+if(score >= 90){
+    console.log("Excellent");
+}else if(score >= 80 && score <= 89){
+    console.log("Good");
+}else if(score >= 70 && score <= 79){
+    console.log("Fair");
+}else{
+    (score <= 70)
+    console.log("Need Improvement")
+}
