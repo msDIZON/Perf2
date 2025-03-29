5d52fa0afd32e053bf2997c1b2ca24fe7842576a
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 19:15:16 +0800

    Create ageCheck.js

---
+++ ageCheck.js
@@ -0,0 +1,16 @@
+// IF STATEMENT ACTIVITY
+
+// Create a program that prompts the user to enter their age
+
+let age = prompt("Enter your Age:");
+
+// Use an if-else structure to determine if the user is child, teenager or already and adult 
+
+if(age <= 13){
+   console.log("Still a Child.");
+}else if(age >= 13 && age <= 19){
+   console.log("Still a Teenager.");
+}else{
+   (age >= 20)
+   console.log("Already an Adult.");
+}
