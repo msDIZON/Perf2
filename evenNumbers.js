d5f6c901d7f856967a1f761b3adbe2a13858637b
msDIZON <jasminedizon0728@gmail.com>
2025-03-29 19:17:06 +0800

    Create evenNumbers.js

---
+++ evenNumbers.js
@@ -0,0 +1,13 @@
+// FOR LOOP ACTIVITY
+
+// Write a program that prints numbers from 1 to 10 using a for loop
+
+let j = 1;
+
+// Modify the program to print only even numbers between 1 and 10, then log it to the console
+
+for(let j=1; j<=10; j++){
+    if(j % 2 === 0){
+        console.log(j)
+    }
+}
